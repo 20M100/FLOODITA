acd9452a89ecff1225423bc269b938bc363e4581
Vincent Mariller <vincent.mariller@etu.sorbonne-universite.fr>
2020-06-01 04:37:13 +0200

    Vrai exo 2 non fonctionnel

--- src/Entete_Fonctions.h
+++ src/Entete_Fonctions.h
@@ -8,13 +8,13 @@
    et met -1 dans ces cases */
 void trouve_zone_rec(int **M, int nbcase, int i, int j, int *taille, ListeCase *L);
 
-/* Même chose qu'au-dessus avec un pointeur B vers une ListeCase qui contient la liste des bords de la zone à un instant donné et un int qui
- * sera affecté la valeur de la couleur de i,j */
-void trouve_zone_imp(int **M, int nbcase, int i, int j, int *taille, ListeCase *L, ListeCase *B, int cl);
+/* Version dérécursifiée */
+void trouve_zone_imp(int **M, int nbcase, int i, int j, int *taille, ListeCase *L);
 
 /* Algorithme tirant au sort une couleur: il utilise la fonction recursive pour determiner la Zsg */
 int sequence_aleatoire_rec(int **M, Grille *G, int dim, int nbcl, int aff);
 
+/* Version qui utilise trouve_zone_imp */
 int sequence_aleatoire_imp(int **M, Grille *G, int dim, int nbcl, int aff);
 
 #endif  /* __ENTETE_FONCTIONS__ */
--- src/S_Zsg.c
+++ src/S_Zsg.c
@@ -7,8 +7,16 @@ void init_Zsg(S_Zsg zone, int dim, int nbcl){
     zone.nbcl = nbcl;
     zone.Lzsg = NULL;
     zone.B = (ListeCase *) malloc(nbcl * sizeof(ListeCase));
-    int i;
+    int i, j;
     for (i = 0; i < nbcl; i++) zone.B[i] = NULL;
+
+    for (i = 0; i < dim; i++){
+
+        for (j = 0; j < dim; j++){
+
+            zone.App[i][j] = -2;
+        }
+    }
 }
 
 /* Ajoute un element dans la liste Lzsg */
@@ -24,22 +32,12 @@ void ajoute_bordure(S_Zsg zone, int i, int j, int cl){
 
 /* Renvoie vrai si une case est dans Lzsg */
 int appartient_Zsg(S_Zsg zone, int i, int j){
-    ListeCase n = zone.Lzsg;
-    while (n != NULL){
-        if (n->i == i && n->j == j) return 1;
-        n = n->suiv;
-    }
-    return 0;
+    return (zone.App[i][j] == -1);
 }
 
 /* Renvoie vrai si une case est dans la bordure de couleur cl donnée */
 int appartient_Bordure(S_Zsg zone, int i, int j, int cl){
-    ListeCase n = zone.B[cl];
-    while (n != NULL){
-        if (n->i == i && n->j == j) return 1;
-        n = n->suiv;
-    }
-    return 0;
+    return (zone.App[i][j] == cl);
 }
 
 /* Met à jour les champs Lzsg et B de Z lorsqu'une case k,l de couleur cl, qui est dans la bordure B[cl], doit basculer dans Lzsg */
